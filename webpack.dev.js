49a20addbbb0aa6c1644ec6a2fe8bcce7500563d
Maksim Shabanski <shabanskij.maksim@gmail.com>
2020-01-07 19:12:23 +0300

    chore: add bundle analyzer

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,83 +1,89 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "js/app.bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'js/app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
     historyApiFallback: {
       rewrites: [
-        { from: /^\/policy/, to: "/policy.html" },
-        { from: /^\/terms/, to: "/terms.html" },
-        { from: /./, to: "/index.html" }
-      ]
-    }
+        { from: /^\/policy/, to: '/policy.html' },
+        { from: /^\/terms/, to: '/terms.html' },
+        { from: /./, to: '/index.html' },
+      ],
+    },
   },
   module: {
     rules: [
       {
         test: /\.js$/i,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
       },
       {
         test: /\.s?css$/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "images/"
-            }
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
           },
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
               disable: true,
               mozjpeg: {
                 progressive: true,
-                quality: 65
+                quality: 65,
               },
               optipng: {
-                enabled: true
+                enabled: true,
               },
               pngquant: {
                 quality: [0.65, 0.9],
-                speed: 4
+                speed: 4,
               },
               gifsicle: {
-                interlaced: false
+                interlaced: false,
               },
               webp: {
-                quality: 75
-              }
-            }
-          }
-        ]
-      }
-    ]
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "public/index.html"
+      filename: 'index.html',
+      template: 'public/index.html',
     }),
     new HtmlWebpackPlugin({
-      filename: "policy.html",
-      template: "public/policy.html"
+      filename: 'policy.html',
+      template: 'public/policy.html',
     }),
     new HtmlWebpackPlugin({
-      filename: "terms.html",
-      template: "public/terms.html"
-    })
-  ]
+      filename: 'terms.html',
+      template: 'public/terms.html',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+      generateStatsFile: true,
+      statsOptions: { source: false },
+    }),
+  ],
 };
